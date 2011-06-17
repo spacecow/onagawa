2d58ff9e3bf71a015fca6b23ab3c3fe023e22e64
Johan Sveholm <jsveholm@gmail.com>
2011-06-17 10:59:55 +0900

    comment yaml

--- config/initializers/i18n_backend.rb
+++ config/initializers/i18n_backend.rb
@@ -1,4 +1,4 @@
-$redis = Redis.new(:host => 'localhost', :port => 6379)
+$redis = Redis.new
 
 I18n.backend = I18n::Backend::Chain.new(I18n::Backend::KeyValue.new($redis), I18n.backend)
 #I18n.backend = I18n::Backend::KeyValue.new($redis)
